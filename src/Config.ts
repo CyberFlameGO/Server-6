b5d7cd5ea1507a2a43dd194148adbc47085c5345
Anatole <anatole@xiantis.net>
2021-02-28 10:34:49 +0100

    Config - allow untyped access

--- src/Config.ts
+++ src/Config.ts
@@ -2,4 +2,10 @@ import Cfg from '@Config';
 
 export const Config = {
 	...Cfg
-};
+} as ConfigType ;
+
+type TypeofCfg = typeof Cfg;
+interface ConfigType extends TypeofCfg {
+	[x: string]: any;
+	[x: number]: any;
+}
